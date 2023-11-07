aed01b44e3a2ba699e7ca86f7b2504bb8a42f60d
Hsword <swordonline@foxmail.com>
2023-11-07 07:45:12 -0500

    add Galvatron
    Co-authored-by: Hsword <swordonline@foxmail.com>
    Co-authored-by: zshCuanNi <zsh2000911@outlook.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,5 @@
 """Cofigure local testing."""
+from lightning_utilities import module_available
+
+if not module_available("galvatron"):
+    raise ModuleNotFoundError("Galvatron is not installed!")
---
+++ tests/helper.py
@@ -0,0 +1,26 @@
+import json
+import os
+import shlex
+import subprocess
+
+
+_PATH_TESTS_DIR = os.path.dirname(__file__)
+TEST_SCRIPT = os.path.join(_PATH_TESTS_DIR, "train.py")
+
+
+# By calling
+def _run_galvatron(trainer_options, strategy_options):
+    """
+    This function executes `tests/train.py::run_test_from_config` in a new subprocess
+    The behavior influences the pytest coverage report, because coverage cannot track what subprocess did
+    """
+    cmdline = [
+        "python3",
+        TEST_SCRIPT,
+        "--trainer-options",
+        shlex.quote(json.dumps(trainer_options)),
+        "--strategy-options",
+        shlex.quote(json.dumps(strategy_options)),
+    ]
+    exit_code = subprocess.call(" ".join(cmdline), shell=True, env=os.environ.copy())
+    assert exit_code == 0
